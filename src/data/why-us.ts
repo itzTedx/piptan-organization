84d6a76f6ff528c0080175a9741c3fe2c44eae6f
Ziron Media <dev@zironmedia.com>
2026-02-13 13:27:03 +0400

    feat(page): add "Why Choose Us?" section with dynamic content

---
+++ src/data/why-us.ts
@@ -0,0 +1,56 @@
+import { IconBuilding } from "@/assets/icon/buildings";
+
+export const WHY_US = [
+	{
+		id: "left",
+		lists: [
+			{
+				id: "01",
+				title: "Extensive Knowledge",
+				description:
+					"Foresee the pain trouble all that rationally encounter to the claims of the obligations of business it will frequently occur.",
+				icon: IconBuilding,
+			},
+			{
+				id: "02",
+				title: "Team Approach",
+				description:
+					"How all this mistaken idea any denouncing pleasure to the claims of the obligations of business it will frequently occur.",
+				icon: IconBuilding,
+			},
+			{
+				id: "03",
+				title: "Time Savings",
+				description:
+					"Actual teachings of the great it explorer of the truth to the claims of the obligations of business it will frequently occur.",
+				icon: IconBuilding,
+			},
+		],
+	},
+	{
+		id: "right",
+		lists: [
+			{
+				id: "04",
+				title: "Risk Management",
+				description:
+					"Foresee the pain trouble all that rationally encounter to the claims of the obligations of business it will frequently occur.",
+				icon: IconBuilding,
+			},
+			{
+				id: "05",
+				title: "Advanced Technology",
+				description:
+					"How all this mistaken idea any denouncing pleasure to the claims of the obligations of business it will frequently occur.",
+				icon: IconBuilding,
+			},
+			{
+				id: "06",
+				title: "Financial Planning",
+				description:
+					"Actual teachings of the great it explorer of the truth to the claims of the obligations of business it will frequently occur.",
+				icon: IconBuilding,
+			},
+		],
+	},
+];
